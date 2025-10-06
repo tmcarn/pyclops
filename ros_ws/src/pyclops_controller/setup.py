308b5b9f88cf00eb953b8e4dba828f388ba8b20c
tmcarn <theo_mcarn@brown.edu>
2025-10-05 22:43:32 -0400

    init commit

---
+++ ros_ws/src/pyclops_controller/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'pyclops_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='theo',
+    maintainer_email='theo@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "drive_node = pyclops_controller.drive_controller_node:main",
+            "teleop_node = pyclops_controller.manual_steering_node:main"
+        ],
+    },
+)
